a2b9e2020c38cf5eb33ecfa2581cdfa09d4127c1
Stacia Carr <staciacarr@Stacia-Carrs-MacBook-Pro-2.local>
2013-08-31 16:39:47 -0700

    initial commit

---
+++ vidnex/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vidnex::Application.config.secret_key_base = 'b0153dbc4f295c72dcaa84f3f356e0cff0d84488ca03604ce2d2f1df2fc7fd4e222c218b94fe13bad3b39d2bfc9b8185b8b11e017f2f90f28c77c22f6c23b684'
